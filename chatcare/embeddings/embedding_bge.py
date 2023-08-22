c6fad3e1d6e3905f33e91f57de21a0e3288077ad
weiliang <weiliang.chong@day-care.cn>
2023-08-22 15:16:49 +0800

    add bge embedding model instance as global

---
+++ chatcare/embeddings/embedding_bge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from FlagEmbedding import FlagModel
+from chatcare.config import params
+
+
+def get_bge(msize):
+    assert msize in ['large', 'base', 'small']
+    model_path = params.bge_model_path
+    bge = FlagModel(
+        model_path,
+        query_instruction_for_retrieval="为这个句子生成表示以用于检索相关文章：",
+        device='cpu',
+    )
+    return bge
+
+
+'''
+form this import bge
+use bge.encode_queries(text) for better classification
+'''
+bge = get_bge('base')
