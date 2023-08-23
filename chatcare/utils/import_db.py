301f8871394d39550f2db786dacbf0992c3a328a
yixuan.liu <yixuan@pve.inai.yijiacare.com>
2023-08-23 17:06:53 +0800

    import excel to milvus

--- chatcare/utils/import_db.py
+++ chatcare/utils/import_db.py
@@ -10,35 +10,81 @@ from chatcare.embeddings.embedding_bge import bge
 
 _HOST = '127.0.0.1'
 _PORT = '19530'
+_VECTOR_FIELD_NAME = 'vector_field'
+_DIM = 768
+_METRIC_TYPE = 'L2'
+_INDEX_TYPE = 'HNSW'
+_M = 32
+_EFCONSTRUCTION = 128
+_EF = 16
+_NPROBE = 16
+_TOPK = 3
 
-# # Create a Milvus connection
-# def create_connection():
-#     print(f"\nCreate connection...")
-#     connections.connect(host=_HOST, port=_PORT)
-#     print(f"\nList connections:")
-#     print(connections.list_connections())
+
+# Create a Milvus connection
+def create_connection():
+    print(f"\nCreate connection...")
+    connections.connect(host=_HOST, port=_PORT)
+    print(f"\nList connections:")
+    print(connections.list_connections())
 
 
-def connect_db(name):
-    if utility.has_collection(name):
-        collection = Collection(name)
-    else:
-        print(f'No database named {name}')
+def get_entity_num(collection):
+    print("\nThe number of entity:")
+    print(collection.num_entities)
+
+
+def create_index(collection, filed_name):
+    index_param = {
+        "index_type": _INDEX_TYPE,
+        "params": {"M": _M, "efConstruction": _EFCONSTRUCTION},
+        "metric_type": _METRIC_TYPE}
+    collection.create_index(filed_name, index_param)
+    print("\nCreated index:\n{}".format(collection.index().params))
+
+
+
+# Create a collection named 'demo'
+def create_collection(collection_name):
+    field_id = FieldSchema(name="id", dtype=DataType.INT64,
+                           description="int64", is_primary=True)
+    field_vec = FieldSchema(name=_VECTOR_FIELD_NAME,
+                            dtype=DataType.FLOAT_VECTOR,
+                            description="float vector", dim=_DIM,
+                            is_primary=False)
+    field_q = FieldSchema(name="question",
+                          dtype=DataType.VARCHAR, max_length=64)
+    field_a = FieldSchema(name="answer",
+                          dtype=DataType.VARCHAR, max_length=8192)
+    schema = CollectionSchema(fields=[field_id, field_vec, field_q, field_a],
+                              description="Care QA knowledge")
+    collection = Collection(name=collection_name, data=None, schema=schema)
+    print("\ncollection created:", collection_name)
 
     return collection
 
 
-def import_data(excel_file, import_mode, name):
-    col = connect_db(name)
+def connect_db(name):
+    create_connection()
+    if utility.has_collection(name):
+        collection = Collection(name)
+    else:
+        collection = create_collection(name)
+    #collection.load()
+    return collection
+
+
+def import_data(collection, excel_file, import_mode):
     id_start = 0
     excel_data = pd.read_excel(excel_file)
 
     if import_mode == "append":
         # get the latest id when appending
-        if not col.is_empty:
-            id_start = col.query(expr="id>=0")[-1]['id']
+        if not collection.is_empty:
+            collection.load()
+            id_start = collection.query(expr="id>=0")[-1]['id']
     elif import_mode == "all":
-        col.delete()
+        collection.delete()
 
     questions = list(excel_data['question'])
     data = [
@@ -47,11 +93,14 @@ def import_data(excel_file, import_mode, name):
         questions,                                          # question
         list(excel_data['answer'])                          # answer
     ]
-    col.insert(data)
-    col.flush()
-    print("Finish inserting")
+    collection.insert(data)
+    collection.flush()
 
-    return 0
+    if id_start == 0:
+        get_entity_num(collection)
+        create_index(collection, _VECTOR_FIELD_NAME)
+
+    print("Finish inserting")
 
 
 if __name__ == '__main__':
@@ -63,5 +112,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     # print(args)
-    import_data(args.excel_file, args.import_mode, args.db_name)
+    create_connection()
+    collection = connect_db(args.db_name)
 
+    import_data(collection, args.excel_file, args.import_mode)
