cde71382cefd111f27ef429f1488b8634943984e
weiliang <weiliang.chong@day-care.cn>
2023-09-07 18:05:00 +0800

    optimize

--- docker/build.sh
+++ docker/build.sh
@@ -6,14 +6,33 @@ echo "version: $version"
 # build .whl
 sh build_whl.sh
 
+# build hnswlib wheel for docker
+pip wheel hnswlib==0.7.0
+
 # copy all needs to current dir
 cp ../requirements.txt .
 mkdir -p ./workspace/models/
 cp -rL /workspace/knowledge_base ./workspace/
 cp -rL /workspace/models/bge-base-zh ./workspace/models/
 cp -r /workspace/models/embedding_classify.pt ./workspace/models
+
+# generate Docerfile
+cat << EOF > Dockerfile
+FROM python:3.8-slim
+WORKDIR /app
+ENV TZ="Asia/Shanghai"
+COPY requirements.txt hnswlib-0.7.0-cp38-cp38-linux_x86_64.whl chatcare-$version-cp38-cp38-linux_x86_64.whl /app/
+RUN pip install -i https://mirrors.aliyun.com/pypi/simple/ --no-cache-dir -r requirements.txt \
+    && pip install --no-cache-dir hnswlib-0.7.0-cp38-cp38-linux_x86_64.whl chatcare-$version-cp38-cp38-linux_x86_64.whl \
+    && rm hnswlib-0.7.0-cp38-cp38-linux_x86_64.whl chatcare-$version-cp38-cp38-linux_x86_64.whl
+
+COPY workspace /workspace
+
+CMD [ "chatcare"]
+EOF
+
 # build docker
-docker build --build-arg version=$version -t chatcare:$version .
+docker build -t chatcare:$version .
 # if meet the Error:
 #     W: GPG error: http://mirrors.tuna.tsinghua.edu.cn/debian buster InRelease: At least one invalid signature was encountered.
 #     E: The repository 'http://mirrors.tuna.tsinghua.edu.cn/debian buster InRelease' is not signed.
