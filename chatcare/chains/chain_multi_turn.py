f3f9e2e3ff0582f2279075b78afa29f8e899b4a6
weiliang <weiliang.chong@day-care.cn>
2023-10-10 10:07:27 +0800

    add chain_multi_turn.py for multi-turn dialogue on database

---
+++ chatcare/chains/chain_multi_turn.py
@@ -0,0 +1,12 @@
+def chain(query):
+    '''
+    query -> entity recgonition -> classify -> search -> answer
+    '''
+    return 'TODO'
+
+
+if __name__ == "__main__":
+    query = '老年痴呆/阿尔茨海默的老人怎么照护？'
+    answer = chain(query)
+    print(f'{query = }')
+    print(f'{answer = }')
