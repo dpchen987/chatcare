ad34e7ffe059d374adc6b8933da30e00c0768be1
weiliang <weiliang.chong@day-care.cn>
2023-10-11 13:03:54 +0800

    add label

--- chatcare/chains/intention.py
+++ chatcare/chains/intention.py
@@ -1,10 +1,16 @@
-intentions = [
-    {
+# key: label of classification
+# value: intention meta
+intentions = {
+    0: {
+        'intent': '无关',
+        'slots': []
+    },
+    1: {
         'intent': '查询疾病护理方案',
         'slots': ['疾病名称', '治疗方式']
     },
-    {
+    2: {
         'intent': '查询护理操作',
         'slots': ['操作名称']
     }
-
+}
