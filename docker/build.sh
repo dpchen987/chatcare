d5cbcd0b8d3a1f12163f5dcef763babdf603fd95
weiliang <weiliang.chong@day-care.cn>
2023-11-30 11:44:55 +0800

    change for SSE GPT

--- docker/build.sh
+++ docker/build.sh
@@ -2,12 +2,19 @@
 
 version=$(python get_version.py)
 echo "version: $version"
+pyversion=310
 
 # build .whl
 sh build_whl.sh
 
 # build hnswlib wheel for docker
-hnswlib_whl=hnswlib-0.7.0-cp38-cp38-linux_x86_64.whl
+hnswlib_whl=hnswlib-0.7.0-cp$pyversion-cp$pyversion-linux_x86_64.whl
+chatcare_whl=chatcare-$version-cp$pyversion-cp$pyversion-linux_x86_64.whl
+torch_whl=torch-2.1.1+cpu-cp$pyversion-cp$pyversion-linux_x86_64.whl
+if [ -f "$torch_whl" ]; then
+  echo "has $torch_whl"
+else
+  wget https://download.pytorch.org/whl/cpu/torch-2.1.1%2Bcpu-cp38-cp38-linux_x86_64.whl -O $torch_whl
 if [ -f "$hnswlib_whl" ]; then
   echo "has $hnswlib_whl"
 else
@@ -17,11 +24,11 @@ fi
 # generate local workspace
 cp ../requirements.txt .
 # mkdir -p ./workspace/models/
-cp -rLp /workspace/knowledge_base ./workspace/
-cp -rLp /workspace/models/bge-base-zh ./workspace/models/
-cp -rp /workspace/models/embedding_classify.pt ./workspace/models
-cp -rp /workspace/models/intention_classify.pt ./workspace/models
-chmod -R 777 workspace
+##cp -rLp /workspace/knowledge_base ./workspace/
+##cp -rLp /workspace/models/bge-base-zh ./workspace/models/
+##cp -rp /workspace/models/embedding_classify.pt ./workspace/models
+##cp -rp /workspace/models/intention_classify.pt ./workspace/models
+##chmod -R 777 workspace
 # generate Docerfile
 # cat << EOF > Dockerfile
 # FROM python:3.8-slim
@@ -35,14 +42,14 @@ chmod -R 777 workspace
 # CMD [ "chatcare"]
 # EOF
 cat << EOF > Dockerfile
-FROM python:3.8-slim
+FROM python:3.10-slim
 WORKDIR /app
 ENV TZ="Asia/Shanghai"
 COPY requirements.txt /app/
-RUN pip install -i https://mirrors.aliyun.com/pypi/simple/ --no-cache-dir -r requirements.txt
-COPY $hnswlib_whl chatcare-$version-cp38-cp38-linux_x86_64.whl /app/
-RUN pip install --no-cache-dir $hnswlib_whl chatcare-$version-cp38-cp38-linux_x86_64.whl \
- && rm $hnswlib_whl chatcare-$version-cp38-cp38-linux_x86_64.whl
+COPY $hnswlib_whl $chatcare_whl $torch_whl /app/
+RUN pip install -i https://mirrors.aliyun.com/pypi/simple/ --no-cache-dir $hnswlib_whl $torch_whl $chatcare_whl \
+ && rm $hnswlib_whl $chatcare_whl $torch_whl \
+ && pip install -i https://mirrors.aliyun.com/pypi/simple/ --no-cache-dir -r requirements.txt
 
 CMD [ "chatcare"]
 EOF
@@ -56,9 +63,9 @@ docker build -t chatcare:$version .
 #     docker container prune
 
 # save docker image, give it to IT guys for deploying
-docker save -o image-chatcare-$version-`date "+%Y-%m-%d_%H.%M.%S"`.tar chatcare:$version
+# docker save -o image-chatcare-$version-`date "+%Y-%m-%d_%H.%M.%S"`.tar chatcare:$version
 
 # clear
-rm chatcare-$version-cp*-cp*-linux_x86_64.whl
+# rm chatcare-$version-cp*-cp*-linux_x86_64.whl
 rm requirements.txt
 # rm -rf workspace
