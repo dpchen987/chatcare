18d1f9ddb88b0e222b4d699ebed89951fbd66644
weiliang <weiliang.chong@day-care.cn>
2023-12-05 09:32:46 +0800

    fix

--- docker/build_whl.sh
+++ docker/build_whl.sh
@@ -4,14 +4,14 @@
 cd ../
 
 # clean old build files
-rm -rf build dist chatcare.egg-info chatcare/*\.c docker/chatcare-*-linux_x86_64.whl
+rm -rf build dist chatcare2.egg-info chatcare2/*\.c docker/chatcare2-*-linux_x86_64.whl
 
 # build
 python setup.py bdist_wheel
-cp dist/chatcare-*-linux_x86_64.whl docker
+cp dist/chatcare2-*-linux_x86_64.whl docker
 
 # clean build files
-rm -rf build dist chatcare.egg-info
-find chatcare/ -type f -name '*.c' -delete
+rm -rf build dist chatcare2.egg-info
+find chatcare2/ -type f -name '*.c' -delete
 # install local whl
-#pip install --force-reinstall docker/chatcare-*-linux_x86_64.whl
+#pip install --force-reinstall docker/chatcare2-*-linux_x86_64.whl
